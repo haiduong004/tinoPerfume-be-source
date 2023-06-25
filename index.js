5340e31345b1edd164a76f24426c0e8e9e042189
Cao Hai Duong <caohaiduong04@gmail.com>
2023-06-25 22:27:24 +0700

    update

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const path = require("path");
 const {sequelize} = require('./models');
 const { customerRouter } = require("./routes/customer.route");
 const { rootRouter } = require("./routes");
+const cors = require('cors');
 
 app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 const publicPathDirection = path.join(__dirname,"./publuc");
 app.use(express.static(publicPathDirection));
